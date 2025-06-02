90323d1160aa8cdb24971b86c12ecdcd69a6b267
Irrazional <70615953+Irrazional@users.noreply.github.com>
2025-06-02 12:30:01 +0700

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/AiScReam-MVP",
+  base: "/AiScReam-MVP/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
