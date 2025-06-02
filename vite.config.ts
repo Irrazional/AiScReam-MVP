fefbaf335b79e15e196fab81609eeb4f6694033e
Irrazional <70615953+Irrazional@users.noreply.github.com>
2025-06-02 12:19:40 +0700

    Deploying to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/AiScReam-MVP",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
